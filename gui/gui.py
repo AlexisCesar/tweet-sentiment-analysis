32b68e3a9a539ae913030b6a32cbab6eb96a4dc2
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-03-19 18:27:38 -0300

    Removed comments

--- gui/gui.py
+++ gui/gui.py
@@ -1,22 +1,16 @@
-from itertools import count
-import PySimpleGUI as psg
-import sys
-from numpy import result_type
-from pymongo import MongoClient
 from classifiers_commons import clearTextList
-import pandas as pd
-from lr import LogisticRegressionClassifier
 from svm import SupportVectorMachineClassifier
+from lr import LogisticRegressionClassifier
 from nb import NaiveBayesClassifier
 from api_secrets import Secrets
+import PySimpleGUI as psg
 import tweepy
+import sys
 
-# GUI definition
 class Window:
     def __init__(self):
         psg.theme('Reddit')
 
-        # Layout
         layout = [
             [psg.Text('Palavra-Chave (Nome, Pessoa, Empresa, Hashtag, Usuário...)', size=(100,0))], 
             [psg.Input(size=(35,0), key='keyword')],
@@ -32,7 +26,6 @@ class Window:
             [psg.Output(size=(50, 10), key='output')]
         ]
 
-        # Window
         self.window = psg.Window("Twitter Sentiment Analysis", size=(800, 600)).layout(layout)
 
     def start(self):
@@ -60,8 +53,6 @@ class Window:
                 maxTweetCount = int(self.values['maxTweetCount'])
                 print(f'Buscando {maxTweetCount} tweets sobre: \'{keyword}\'')
                 
-                # Call a function to retrieve the tweets and store into a list
-                # tweets_found = ['@pessoaX eu te amo', '@pessoaY eu te odeio', 'livro muito bom #gostei', 'livro excelente https://livro.com', 'jogo muito ruim']
                 tweets_found = []
                 for tweet in tweepy.Cursor(twitter_api.search_tweets, q=keyword + '-filter:retweets',
                                             count=maxTweetCount,
@@ -69,12 +60,9 @@ class Window:
                                             tweet_mode='extended',
                                             lang="pt").items(maxTweetCount):
                     tweets_found.append(tweet.full_text)
-                # print(tweets_found)
-                # Clean tweets with data preparation steps
+
                 tweets_found = clearTextList(tweets_found)
-                # print(tweets_found)
                 
-                # Perform classification of each list item and store in pos/neg counters
                 total_predicted_as_positive = total_predicted_as_negative = 0
                 for tweet in tweets_found:
                     predictions = []
@@ -82,15 +70,12 @@ class Window:
                     predictions.append(lr_classifier.classifyText(tweet))
                     predictions.append(svm_classifier.classifyText(tweet))
                     predictions.append(nb_classifier.classifyText(tweet))
-                    # print(predictions)
 
-                    # Voting
                     if predictions.count(0) > predictions.count(1):
                         total_predicted_as_negative = total_predicted_as_negative + 1
                     else:
                         total_predicted_as_positive = total_predicted_as_positive + 1
 
-                # Inform how many are classified as pos and how many as neg, with their percentage
                 total_predicted = total_predicted_as_negative + total_predicted_as_positive
 
                 print(f"Classificados com sentimento negativo: {total_predicted_as_negative} ({total_predicted_as_negative / total_predicted * 100}%)")
